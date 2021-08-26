8069127e13383b088caf4aaa9342f10fd0b782f2
Alexandre-SB <salzgeber10@gmail.com>
2021-08-26 18:00:51 -0300

    Checkpoint1

---
+++ microondasGrupo7.js
@@ -0,0 +1,66 @@
+// Aula 09 - 26/08/2021
+// Checkpoint - Microondas
+// Grupo 7 - 
+
+
+/* 1 ͦ Checkpoint de Programação Imperativa!
+Olá, você foi contratado para executar este projeto. É importante que você analise, entenda o pedido do cliente e desenvolva conforme solicitado. Veja abaixo os requisitos do projeto:
+
+- Precisamos desenvolver um menu para um microondas super veloz, onde teremos 5 opções de comida com seus respectivos tempos pré-definidos. 
+
+      1 - Pipoca – 10 segundos (padrão);
+      2 - Macarrão – 8 segundos (padrão);
+      3 - Carne – 15 segundos (padrão);
+      4 - Feijão – 12 segundos (padrão);
+      5 - Brigadeiro – 8 segundos (padrão); 
+
+- O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
+- Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
+- Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
+- Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
+- No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!". */
+
+console.log("||||||||||||||||||||||||||||||");
+console.log("||||||||| Microondas |||||||||");
+console.log("||||||||||||||||||||||||||||||");
+console.log("1 - Pipoca – 10 segundos (padrão)");
+console.log("2 - Macarrão – 8 segundos (padrão)");
+console.log("3 - Carne – 15 segundos (padrão)");
+console.log("4 - Feijão – 12 segundos (padrão)");
+console.log("5 - Brigadeiro – 8 segundos (padrão)");
+console.log("0 - Tempo-padrao");
+console.log("Tecle a opção desejada e o seu respectivo tempo de preparo. Tecle 0 para utilizar o tempo-padrão");
+
+
+var prato = "";
+var tmp = "";
+var tmpPadrao = 0;
+var result = "";
+
+function microondas(opcao) {
+
+if (option == 1) {
+      optUser = "PIPOCA"      
+} else if (option == 2) {
+      optUser = "MACARRÃO"      
+} else if (option == 3) {
+      optUser = "CARNE"      
+} else if (option == 4) {
+      optUser = "FEIJÃO"      
+} else if (option == 5) {
+      optUser = "BRIGADEIRO"
+} else {
+      return console.log("Opção inválida, escolha outra")
+}
+return result
+}
+
+microondas(1)
+
+switch (optUser) {
+      case "PIPOCA":
+      console.log("Você escolheu PIPOCA")
+      console.log("Tempo de praparo = 10 segundos")
+      console.log("Seu prato já está pronto!")
+      break;
+}
